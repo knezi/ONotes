6168d20548a85957ff3e47f94fb506ae70479127
Jan Knížek <jk@janknizek.cz>
2015-09-18 21:45:27 +0200

    made test build

--- make.sh
+++ make.sh
@@ -1,6 +1,15 @@
 #!/bin/env sh
 npm install
+rm -r build testbuild
 mkdir build -p
 cp src/* build/ -R
 cp node_modules/angular/angular.min.js build/js/lib/
 cp node_modules/angular-route/angular-route.min.js build/js/lib
+pushd build > /dev/null
+zip ONotes.zip * -r > /dev/null
+popd > /dev/null
+cp build testbuild -R
+sed 's/O Notes/O Notes-testing/' testbuild/manifest.webapp -i
+#for  x in testbuild/icons/icon*; do
+#	mogrify -annotate 200x200 hoj $x
+#done
