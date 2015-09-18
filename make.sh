9655902774bd658e64a4a0b3e0e56c69387d4577
Jan Knížek <jk@janknizek.cz>
2015-09-18 21:49:45 +0200

    testbuild red header

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
-npm install
+#npm install
 rm -r build testbuild
 mkdir build -p
 cp src/* build/ -R
@@ -10,6 +10,9 @@ zip ONotes.zip * -r > /dev/null
 popd > /dev/null
 cp build testbuild -R
 sed 's/O Notes/O Notes-testing/' testbuild/manifest.webapp -i
+echo "#header {\
+	background: red;\
+}" >> testbuild/css/main.css
 #for  x in testbuild/icons/icon*; do
 #	mogrify -annotate 200x200 hoj $x
 #done
